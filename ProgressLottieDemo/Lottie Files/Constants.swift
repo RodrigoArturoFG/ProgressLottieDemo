63bc2693acfe262a91a0ab19cfdd8979acf458c1
RodrigoArturoFG <rodrigoarturofg@gmail.com>
2024-10-08 11:41:17 -0600

    Added more options for loading lottie

--- ProgressLottieDemo/Lottie Files/Constants.swift
+++ ProgressLottieDemo/Lottie Files/Constants.swift
@@ -9,4 +9,11 @@ import Foundation
 
 enum Constants {
     static var greenLoading = "Green Loading"
+    static var threeLines = "Three Lines"
+    static var circleAnimation = "Circle Animation"
+    static var circleAnimationTwo = "Circle Animation 2"
+    static var LoadingAnimation = "Loading Animation"
+    static var LoadingAnimationTwo = "Loading Animation 2"
+    static var LoadingAnimationThree = "Loading Animation 3"
+    static var CircleLoadingAnimation = "Circle Loading Animation"
 }
